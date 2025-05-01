93202e2b7aa909e583f8b3dd4699b1b0840b8eef
Vijay Ram Raghuraman <vijay.raghuraman@hitachids.com>
2025-05-02 00:53:30 +0530

    feat(db): setup engine, db and session

---
+++ backend/db/engine.py
@@ -0,0 +1,27 @@
+"""SQLModel engine for the checklist app."""
+
+from typing import Annotated, Generator
+
+from fastapi import Depends
+from sqlmodel import Session, SQLModel, create_engine
+
+from models.checklist import Checklist
+
+
+DB_URL = "sqlite:///./checklists.sqlite"
+engine = create_engine(DB_URL, connect_args={"check_same_thread": False})
+SQLModel.metadata.create_all(engine)
+
+
+def get_session() -> Generator[Session, None, None]:
+    """Method to get a session for the database.
+
+    Yields:
+        Generator[Session, None, None]: A session for the database.
+    """
+
+    with Session(engine) as session:
+        yield session
+
+
+DatabaseSession = Annotated[Session, Depends(get_session)]
