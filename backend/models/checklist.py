9d719f0369be964109970966f075775e9affa7ab
Vijay Ram Raghuraman <vijay.raghuraman@hitachids.com>
2025-05-02 00:52:04 +0530

    feat(checklists): add models

---
+++ backend/models/checklist.py
@@ -0,0 +1,21 @@
+"""Definitions of the ORM models for the database tables."""
+
+from typing import Sequence
+from pydantic import BaseModel
+from sqlmodel import SQLModel, Field
+
+from . import AuditMixin, Pagination
+
+
+class Fields(SQLModel):
+    title: str | None = Field(default=None, max_length=31)
+    description: str | None = Field(default=None, max_length=255)
+
+
+class Checklist(AuditMixin, Fields, table=True):
+    pass
+
+
+class PaginatedChecklists(BaseModel):
+    pagination: Pagination
+    checklists: Sequence[Checklist] = Field(default_factory=list)
