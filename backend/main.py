0e6f1cb2d2f47e2c2dc527111f68f3d6b117c447
Vijay Ram Raghuraman <vijay.raghuraman@hitachids.com>
2025-05-06 20:21:49 +0530

    feat(app): add router for tasks

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,7 @@
 from fastapi import FastAPI
 
 from api.checklist import router as checklist_router
+from api.task import router as task_router
 from db.engine import create_db_and_tables
 
 
@@ -10,3 +11,4 @@ create_db_and_tables()
 
 app = FastAPI()
 app.include_router(checklist_router)
+app.include_router(task_router)
